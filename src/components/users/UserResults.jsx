92fcee1c495a96a709139dd3f58517477d0e3682
Javier Carmona <neomaster95@gmail.com>
2022-03-19 14:31:34 -0600

    Commit Inicial Copiado del Github Finder

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
+import { Spinner } from '../layout/Spinner';
+import { UserItem } from './UserItem';
+export const UserResults = () => {
+  const { users, isLoading } = useContext(GithubContext);
+
+  if (!isLoading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+};
